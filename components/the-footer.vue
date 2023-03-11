a98f926727fbb12822d2d1c439da3d1accde098d
nicodev <ndevaux@gmail.com>
2023-03-11 09:42:36 +0100

    test

---
+++ components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "the-footer"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ components/the-header.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "the-header"
+}
+</script>
+
+<style scoped>
+
+</style>
